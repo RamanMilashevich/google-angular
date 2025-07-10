1c610e2238fad4da0ad2c1077f8d61d5daac1627
RamanMilashevich <roma.milashevich@gmail.com>
2025-07-10 20:25:40 +0200

    feat: add json file to HTTP requests

--- homes-app/src/app/home/home.component.ts
+++ homes-app/src/app/home/home.component.ts
@@ -11,13 +11,13 @@ import { HousingService } from '../housing.service';
   template: `
     <div class="container">
       <section class="form-container">
-        <form class="search-form">
-          <input type="text" placeholder="Filter by city">
-          <button class="primary" type="button">Search</button>
+        <form class="search-form" (submit)="$event.preventDefault()">
+          <input type="text" placeholder="Filter by city" #filter>
+          <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
         </form>
       </section>
       <section class="results">
-        <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+        <app-housing-location *ngFor="let housingLocation of filteredlocationList" [housingLocation]="housingLocation"></app-housing-location>
       </section> 
     </div> 
   `,
@@ -26,8 +26,23 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredlocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredlocationList = housingLocationList;
+      })
   }
+
+  filterResults(text: string) {
+    if(!text) {
+      this.filteredlocationList = this.housingLocationList;
+    }
+
+    this.filteredlocationList= this.housingLocationList.filter(housingLocation => 
+      housingLocation.city.toLowerCase().includes(text.toLowerCase())
+    );
+  } 
 }
